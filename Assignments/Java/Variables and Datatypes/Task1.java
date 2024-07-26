783069b53c9d40c88635c6c3f26996bb8272e684
MD. Alvi Chowdhury <167624337+Balanced0@users.noreply.github.com>
2024-07-26 16:06:44 +0600

    Declaration and initialization

--- Assignments/Java/Variables and Datatypes/Task1.java
+++ Assignments/Java/Variables and Datatypes/Task1.java
@@ -3,6 +3,48 @@ public class Task1{
         int a;
         a = 5;
         System.out.println(a);
+        
+        int b = 10;
+        int sum = a + b;
+        System.out.println(sum);
+        
+        int product = a * b;
+        int division = a / b;
+        System.out.println(product);
+        System.out.println(division);
+        
+        double a1 = 12;
+        double b1 = 13;
+        double sum1 = a1 + b1;
+        double product1 = a1 * b1;
+        double division1 = a1 / b1;
+        System.out.println(a1);
+        System.out.println(b1);
+        System.out.println(sum1);
+        System.out.println(product1);
+        System.out.println(division1);
+        
+        double a2 = 50;
+        int b2 = 40;
+        double sum2  = a2 + b2;
+        int product2 = (int)(a2 * b2);
+        double division2 = a2 / b2;
+        System.out.println(a2);
+        System.out.println(b2);
+        System.out.println(sum2);
+        System.out.println(product2);
+        System.out.println(division2);
+        
+        String s1;
+        s1 = "Hello,";
+        String s2 = "World!";
+        System.out.println(s1);
+        System.out.println(s2);
+        String s_sum  = s1 + s2;
+        System.out.println(s_sum);
+        int integer = 10;
+        String i_s_sum = integer + s2;
+        System.out.println(i_s_sum);
     }
 }
     
---
+++ Assignments/Java/Variables and Datatypes/Task1.java~
@@ -0,0 +1,8 @@
+public class Task1{
+    public static void main(String[] args){
+        int a;
+        a = 5;
+        System.out.println(a);
+    }
+}
+    
