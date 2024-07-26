a231da6561f328a66eb52d2db5d655a7566ac235
MD. Alvi Chowdhury <167624337+Balanced0@users.noreply.github.com>
2024-07-26 16:09:29 +0600

    Write Java code that calculates and prints the circumference and area of a circle with a radius of 4 units.

---
+++ Assignments/Java/Variables and Datatypes/Task2.java
@@ -0,0 +1,11 @@
+public class Task2{
+    public static void main(String[] args){
+        int r = 4;
+        double pi = 3.1416;
+        double circumference = 2 * pi * r;
+        double area = pi * r * r;
+        System.out.println("The circumference of the circle is: " + circumference);
+        System.out.println("The area of the circle is: " + area);
+        
+    }
+}
