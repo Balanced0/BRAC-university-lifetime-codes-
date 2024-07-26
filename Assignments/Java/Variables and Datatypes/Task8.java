d5a75e9011bfe41124bf42ac2c3ce1a836e4a707
MD. Alvi Chowdhury <167624337+Balanced0@users.noreply.github.com>
2024-07-26 16:16:47 +0600

    Write a Java Code to display the multiplication table for a given positive integer 'n'. The table should include the products of 'n' with each of the numbers from 1 to 10.

---
+++ Assignments/Java/Variables and Datatypes/Task8.java
@@ -0,0 +1,25 @@
+public class Task8{
+    public static void main(String[] args){
+        int n = 18;
+        int n1 = 18 * 1;
+        int n2 = 18 * 2;
+        int n3 = 18 * 3;
+        int n4 = 18 * 4;
+        int n5 = 18 * 5;
+        int n6 = 18 * 6;
+        int n7 = 18 * 7;
+        int n8 = 18 * 8;
+        int n9 = 18 * 9;
+        int n10 = 18 * 10;
+        System.out.println(n + " x 1 = " + n1);
+        System.out.println(n + " x 2 = " + n2);
+        System.out.println(n + " x 3 = " + n3);
+        System.out.println(n + " x 4 = " + n4);
+        System.out.println(n + " x 5 = " + n5);
+        System.out.println(n + " x 6 = " + n6);
+        System.out.println(n + " x 7 = " + n7);
+        System.out.println(n + " x 8 = " + n8);
+        System.out.println(n + " x 9 = " + n9);
+        System.out.println(n + " x 10 = " + n10);
+    }
+}
