83e182d461c2251ecb8a24aff8a786d20d515f27
MD. Alvi Chowdhury <167624337+Balanced0@users.noreply.github.com>
2024-07-26 16:19:06 +0600

    Write a Java program that displays the 2 rightmost digits of your student ID in reverse order.

---
+++ Assignments/Java/Variables and Datatypes/Task11.java
@@ -0,0 +1,11 @@
+public class Task11 {
+    public static void main(String[] args){
+        //Task 12
+        int student_id = 24301431;
+        int a1 = student_id%100;
+        int a2 = a1 % 10;
+        System.out.println(a2);
+        int a3 = a1 / 10;
+        System.out.println(a3);
+    }
+}
