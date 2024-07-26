265c1961b88bad2994111ed0cbe9e57468c71479
MD. Alvi Chowdhury <167624337+Balanced0@users.noreply.github.com>
2024-07-26 16:12:11 +0600

    Write a Java program that given a number in inches (you have to declare and initialize it yourself) converts it to meters.

---
+++ Assignments/Java/Variables and Datatypes/Task4.java
@@ -0,0 +1,7 @@
+public class Task4{
+    public static void main(String[] args){
+        int inch = 1000;
+        double meter = 1000 * 0.0254;
+        System.out.println("1000 inch is " + meter + " meters");
+    }
+}
