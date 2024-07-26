2ef93461ce32b5c051f40e24256ebbea612a8737
MD. Alvi Chowdhury <167624337+Balanced0@users.noreply.github.com>
2024-07-26 16:17:44 +0600

    Write the Java code of a program that finds the sum of the first 100 positive numbers.

---
+++ Assignments/Java/Variables and Datatypes/Task9.java
@@ -0,0 +1,9 @@
+public class Task9{
+    public static void main(String[] args){
+        int n = 100;
+        int a = 1;
+        int L = 100;
+        int s = (n/2)*(a + L);
+        System.out.println("The sum of first 100 positiove numbers is " + s);
+    }
+}
