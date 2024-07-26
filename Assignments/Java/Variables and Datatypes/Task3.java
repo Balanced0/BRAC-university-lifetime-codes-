56622d82c53faf086e0acdc1d79c46bdc208bd14
MD. Alvi Chowdhury <167624337+Balanced0@users.noreply.github.com>
2024-07-26 16:10:08 +0600

    Write a Java code where given an integer we need to print the last 2 digits of that number.

---
+++ Assignments/Java/Variables and Datatypes/Task3.java
@@ -0,0 +1,7 @@
+public class Task3{
+    public static void main(String[] args){
+        int integer  = 7867;
+        int last_2_digits = integer % 100;
+        System.out.println("The last two digits are " + last_2_digits);
+    }
+}
