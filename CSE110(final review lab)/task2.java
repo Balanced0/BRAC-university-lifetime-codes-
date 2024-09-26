c39d2c1e021b09073f1dba741c58612dae4282e2
MD. Alvi Chowdhury <167624337+Balanced0@users.noreply.github.com>
2024-09-26 16:28:03 +0600

    Write a method called sphereVolume that takes an integer radius in its parameter and returns the volume of the sphere.

--- CSE110(final review lab)/task2.java
+++ CSE110(final review lab)/task2.java
@@ -5,10 +5,18 @@ public class task2{
         int n = sc.nextInt();
         double area = circleArea(n);
         System.out.printf("%.4f%n", area);
+        double volume = sphereVolume(n);
+        System.out.printf("%.4f%n", volume);
+
     }
     
     public static double circleArea(int r){
         double a = 3.1416 * r * r;
         return a;
+    }
+    
+    public static double sphereVolume(int r){
+        double v = (4.0/3) * 3.1416 * r * r * r;
+        return v;
     }
 }
