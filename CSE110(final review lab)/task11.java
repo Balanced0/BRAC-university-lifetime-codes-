5b08e467c8c83ce9bc203ef0d40c2310e32a7445
MD. Alvi Chowdhury <167624337+Balanced0@users.noreply.github.com>
2024-09-27 22:37:50 +0600

    Create an array of type double of length 5 called cheetos_inches, that stores the length of each of the Cheetos from the user. Send the array of length in inches into a method called findAvgCm() that returns the average length of the Cheetos in cm to 2 decimal places. The method findAvgCm() uses convertTocm() to convert the length of each Cheetos from inches to cm

--- CSE110(final review lab)/task11.java
+++ CSE110(final review lab)/task11.java
@@ -7,10 +7,23 @@ public class task11{
         int n = sc.nextInt();
         double t = convertToCm(n);
         System.out.println(t + " cm");
+        double [] cheetos_inches = new double[]{10.0, 12.0, 14.0, 16.0, 18.0};
+        double averageLength = findAvgCm(cheetos_inches);
+        System.out.printf("The average Cheeto length is %.2f cm\n", averageLength);
     }
     
-    public static double convertToCm(int n){
+    public static double convertToCm(double n){
         double cm = 2.54 * n;
         return cm;
+    }
+    
+    public static double findAvgCm(double [] cheetos_inches){
+        double sum = 0.0;
+        for(int i = 0; i < cheetos_inches.length; i++){
+            double a = convertToCm(cheetos_inches[i]);
+            sum = sum + a;
+        }
+        double avg = sum / (cheetos_inches.length);
+        return avg;
     }
 }
---
+++ CSE110(final review lab)/task11.java~
@@ -0,0 +1,16 @@
+//Arrays + Methods
+//Task 10
+import java.util.Scanner;
+public class task11{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double t = convertToCm(n);
+        System.out.println(t + " cm");
+    }
+    
+    public static double convertToCm(int n){
+        double cm = 2.54 * n;
+        return cm;
+    }
+}
