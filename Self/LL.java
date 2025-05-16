5612bb0265e656039743b7ca6d8614dbee457ca2
MD. Alvi Chowdhury <167624337+Balanced0@users.noreply.github.com>
2025-05-16 18:20:55 +0600

    Created the first linked list

--- Self/LL.java
+++ Self/LL.java
@@ -22,5 +22,22 @@ public class LL{
         this.size = 0;
     }
     
+    public void insertFirst(int val){
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+        if(tail == null){
+            tail = head;
+        }
+        size += 1;
+    }
     
+    public void display(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.print("END");
+    }
 }
--- Self/Main.java
+++ Self/Main.java
@@ -1,5 +1,12 @@
 public class Main{
     public static void main(String[] args){
         LL list = new LL();
+        
+        list.insertFirst(3);
+        list.insertFirst(8);
+        list.insertFirst(9);
+        list.insertFirst(50);
+        list.insertFirst(100);
+        list.display();
     }
 }
